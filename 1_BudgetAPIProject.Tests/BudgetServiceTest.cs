e2c7d4633ddbc624465e77bed6cc22acb1a8b4f1
etta.huang <etta.huang@ailabpte.co>
2024-12-25 17:56:17 +0800

    push

--- 1_BudgetAPIProject.Tests/BudgetServiceTest.cs
+++ 1_BudgetAPIProject.Tests/BudgetServiceTest.cs
@@ -15,9 +15,9 @@ public class BudgetServiceTest
         var budgetRepo = Substitute.For<IBudgetRepo>();
         budgetRepo.GetAll().Returns(new List<Budget>()
         {
-            new Budget(){YearMonth = "202412", Amount = "3100"},
-            
-        })
+            new Budget() { YearMonth = "202412", Amount = 0 },
+            new Budget() { YearMonth = "202501", Amount = 1000 },
+        });
         var budgetService = new BudgetService(budgetRepo);
         var start = new DateTime(2024, 12, 01);
         var end = new DateTime(2024, 12, 31);
